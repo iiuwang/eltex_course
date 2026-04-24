9e339832074550b37932f56fc10c50ee01389631
Alena Alena <afilippova287@gmail.com>
2026-04-24 10:23:21 +0700

    lesson-11-angular2

---
+++ blog-app/src/app/ui/components/form-add-post/form-add-post.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-add-post',
+  imports: [],
+  templateUrl: './form-add-post.html',
+  styleUrl: './form-add-post.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FormAddPost { }
--- blog-app/src/app/ui/pages/blog-page/blog-page.ts
+++ blog-app/src/app/ui/pages/blog-page/blog-page.ts
@@ -1,33 +1,40 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BlogPost } from '../../components/blog-post/blog-post';
+import { FormAddPost } from "../../components/form-add-post/form-add-post";
 
 @Component({
   selector: 'app-blog-page',
-  imports: [BlogPost],
+  imports: [BlogPost, FormAddPost],
   templateUrl: './blog-page.html',
   styleUrl: './blog-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
 export class BlogPage { 
-    posts = [
-    {
-      title: 'Статья 1',
-      description: 'Описание статьи',
-      date: '10 апреля 2026',
-      image: 'selection.png'
-    },
-    {
-      title: 'Статья 2',
-      description: 'Ещё описание',
-      date: '12 апреля 2026',
-      image: 'selection.png'
-    },
-    {
-      title: 'Статья 3',
-      description: 'Ещё описание',
-      date: '12 апреля 2026',
-      image: 'selection.png'
-    }
+  // попытка сделать условное отображенние формы
+  addFormVisible = false;
+  switchForm() {
+    this.addFormVisible = !this.addFormVisible;
+  }
+
+  posts = [
+  {
+    title: 'Статья 1',
+    description: 'Описание статьи 1',
+    date: '10 апреля 2026',
+    image: 'selection.png'
+  },
+  {
+    title: 'Статья 2',
+    description: 'Описание статьи 2',
+    date: '12 апреля 2026',
+    image: 'selection.png'
+  },
+  {
+    title: 'Статья 3',
+    description: 'Описание статьи 3',
+    date: '12 апреля 2026',
+    image: 'selection.png'
+  }
   ];
 }
